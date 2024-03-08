b9d52a988653ef8285d9c42d063898ce9a8fa0b1
k-leong <kenny.leong0221@yahoo.com>
2024-03-07 20:06:04 -0800

    added dockerfile

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import requests
-# import boto3
+import boto3
 
 app = Flask(__name__)
 
@@ -18,7 +18,7 @@ def fetch_random_dog_image():
 # Function to increment visitor count
 def increment_visitor_count():
     table.update_item(
-        Key={'id': '1'},
+        Key={'id': 0},
         UpdateExpression='ADD visitor_count :incr',
         ExpressionAttributeValues={':incr': 1}
     )
@@ -26,7 +26,10 @@ def increment_visitor_count():
 # Function to get current visitor count
 def get_visitor_count():
     response = table.get_item(
-        Key={'id': '1'}
+        Key={'id': 0},
+        AttributesToGet=[
+        'visitor_count',
+        ]
     )
     item = response.get('Item')
     return item.get('visitor_count', 0) if item else 0
@@ -42,8 +45,7 @@ def index():
     # Get current visitor count
     visitor_count = get_visitor_count()
 
-    # return render_template('index.html', dog_image=dog_image, visitor_count=visitor_count)
-    return render_template('index.html', dog_image=dog_image)
+    return render_template('index.html', dog_image=dog_image, visitor_count=visitor_count)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
